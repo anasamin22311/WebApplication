ba9b6f91631052b09d05039b2cb3303d8b9bbb2e
anas fci <anas.fci_1405@fci.kfs.edu.eg>
2022-03-25 06:12:01 +0200

    c

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404: ViewBag.ErrorMessage = "sorry The URL you have requested couldn't be found";
+                        break;
+
+            }
+            return View("NotFound");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,8 @@ namespace WebApplication5
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                //app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
